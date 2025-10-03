44d9d77dc1f69e761c307f42787b9fa9bb193b59
rahimuj570 <rahimujjaman2002@gmail.com>
2025-10-03 21:37:25 +0600

    successfully fetched progress task in progress_screen page

--- lib/ui/screens/task_screens/progress_task_screen.dart
+++ lib/ui/screens/task_screens/progress_task_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:todo_app/data/models/task_model.dart';
+import 'package:todo_app/data/services/api_calller.dart';
+import 'package:todo_app/data/utils/urls.dart';
 import 'package:todo_app/ui/utils/task_status.dart';
 import 'package:todo_app/ui/widgets/task_tile_widget.dart';
 
@@ -12,7 +14,35 @@ class ProgressTaskScreen extends StatefulWidget {
 }
 
 class _ProgressTaskScreenState extends State<ProgressTaskScreen> {
-  final List<TaskModel> _progressTaskList = [];
+  bool isFatching = false;
+  final List<TaskModel> _cancelledTaskList = [];
+
+  Future<void> getCompletedTodo() async {
+    _cancelledTaskList.clear();
+    isFatching = !isFatching;
+    setState(() {});
+    ApiResponse apiResponse = await ApiCalller.getRequest(
+      url: Urls.getTodoList(TaskStatus.Progress.name),
+    );
+
+    if (apiResponse.isuccess == true) {
+      for (Map<String, dynamic> t in apiResponse.responseData['data']) {
+        TaskModel temp = TaskModel.fromJson(t);
+        _cancelledTaskList.add(temp);
+      }
+    }
+
+    isFatching = !isFatching;
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getCompletedTodo();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -21,16 +51,41 @@ class _ProgressTaskScreenState extends State<ProgressTaskScreen> {
         children: [
           SizedBox(height: 10),
           Expanded(
-            child: Material(
-              child: ListView.separated(
-                itemBuilder: (context, index) => TaskTileWidget(
-                  reFetch: () => Future<void>(() {}),
-                  statusColor: Colors.purpleAccent,
-                  tm: _progressTaskList[index],
-                  status: TaskStatus.Progress,
+            child: Visibility(
+              visible: !isFatching,
+              replacement: Center(
+                child: SizedBox(
+                  height: 200,
+                  width: 200,
+                  child: CircularProgressIndicator(strokeWidth: 5),
                 ),
-                separatorBuilder: (context, index) => SizedBox(height: 5),
-                itemCount: _progressTaskList.length,
+              ),
+              child: Material(
+                child: Visibility(
+                  visible: _cancelledTaskList.isNotEmpty,
+                  replacement: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.folder_off, size: 200, color: Colors.green),
+                        Text(
+                          "No Task Found",
+                          style: Theme.of(context).textTheme.bodyLarge,
+                        ),
+                      ],
+                    ),
+                  ),
+                  child: ListView.separated(
+                    itemBuilder: (context, index) => TaskTileWidget(
+                      reFetch: getCompletedTodo,
+                      statusColor: Colors.purple,
+                      status: TaskStatus.Progress,
+                      tm: _cancelledTaskList[index],
+                    ),
+                    separatorBuilder: (context, index) => SizedBox(height: 5),
+                    itemCount: _cancelledTaskList.length,
+                  ),
+                ),
               ),
             ),
           ),
